da108444988a70a0c27a96dad708d3466a2d07a5
syuhada-ramadhan <suhadahada18@gmail.com>
2022-01-12 09:14:39 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// built-in functions
+// alert("Waktunya Belajar");
+prompt("Masukan nama anda:");
+
+// Keuntungan = Harga Penjualan - Harga Pembelian
+function keuntungan(hargaJual, hargaBeli) {
+  return hargaJual - hargaBeli;
+}
+
+// const keuntunganJualKambing = keuntungan(3000, 2000);
+// console.log(keuntunganJualKambing);
+
+const keuntunganJualAyam = keuntungan(1500, 1000);
+console.log(keuntunganJualAyam);
